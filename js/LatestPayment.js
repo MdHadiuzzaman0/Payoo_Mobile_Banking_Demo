0bdfb90f07b74b52a9bf3f76b78ae4e543fa6efe
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-03-20 22:03:51 +0600

    unnecessary file removed

--- js/LatestPayment.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('logout').addEventListener("click", function () {
-            window.location.assign('./index.html')
-        })
-
-        document.getElementById('viewall').addEventListener("click", function () {
-            const update = document.getElementById('update');
-            update.classList.toggle('hidden');
-        })
